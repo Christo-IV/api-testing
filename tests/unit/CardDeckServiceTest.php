e0448b7219e03a8077a2b612a3c25810c3ea56d8
Christo-IV <christo.vahtramae@khk.ee>
2021-05-11 20:59:29 +0300

    Added 2 unit tests

--- tests/unit/CardDeckServiceTest.php
+++ tests/unit/CardDeckServiceTest.php
@@ -8,6 +8,23 @@ use App\Service\CardDeckService;
 
 class CardDeckServiceTest extends TestCase
 {
+    public function testSuits()
+    {
+        $cardDeck = CardDeckService::getShuffledDeck(CardDeckService::getDeck());
+
+        $this->assertStringContainsString('S', join("", $cardDeck));
+        $this->assertStringContainsString('H', join("", $cardDeck));
+        $this->assertStringContainsString('D', join("", $cardDeck));
+        $this->assertStringContainsString('C', join("", $cardDeck));
+    }
+
+    public function testCardArray()
+    {
+        $cardDeck = CardDeckService::getDeck();
+
+        $this->assertIsArray($cardDeck);
+        $this->assertContainsOnly('string', $cardDeck);
+    }
 
     public function testGetDeck()
     {
